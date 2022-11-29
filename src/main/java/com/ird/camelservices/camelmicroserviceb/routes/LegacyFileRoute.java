355cd282e2ce1e341eb5aad7228e507006a54411
aaronchen <aaronchen9157@gmail.com>
2022-11-29 14:37:28 +0800

    file processor with route and unmarshal to POJO

--- src/main/java/com/ird/camelservices/camelmicroserviceb/routes/LegacyFileRoute.java
+++ src/main/java/com/ird/camelservices/camelmicroserviceb/routes/LegacyFileRoute.java
@@ -1,6 +1,8 @@
 package com.ird.camelservices.camelmicroserviceb.routes;
 
+import com.ird.camelservices.camelmicroserviceb.beans.NameAddress;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.dataformat.beanio.BeanIODataFormat;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -9,16 +11,19 @@ import org.springframework.stereotype.Component;
 public class LegacyFileRoute extends RouteBuilder {
 
     Logger logger = LoggerFactory.getLogger(getClass());
-
+    BeanIODataFormat inboundDataformat = new BeanIODataFormat("inboundMessageBeanIOMapping.xml","inputMessageStream");
     @Override
     public void configure() throws Exception {
 
-        from("file:src/data/input?fileName=inputFile.txt")
+        from("file:src/data/input?fileName=inputFile.csv")
                 .routeId("legacyFileMoveRouteId")
+                .split(body().tokenize("\n",1,true))
+                .unmarshal(inboundDataformat)
                 .process(exchange -> {
-                    String filedata = exchange.getIn().getBody(String.class);
-                    logger.info("This is the read fileData: " +filedata);
+                    NameAddress filedata = exchange.getIn().getBody(NameAddress.class);
+                    logger.info("This is the read fileData: " +filedata.toString());
                 })
-                .to("file:src/data/output?fileName=outputFile.txt");
+                //.to("file:src/data/output?fileName=outputFile.csv&fileExist=append&appendChars=\\n")
+                .end();
     }
 }
