e608a15994dc8ed03dcda4c23e24d481ecf37cf8
aaronchen <aaronchen9157@gmail.com>
2022-11-29 10:00:45 +0800

    udemy timer set

--- src/main/java/com/ird/camelservices/camelmicroserviceb/routes/SimpleTimer.java
+++ src/main/java/com/ird/camelservices/camelmicroserviceb/routes/SimpleTimer.java
@@ -8,9 +8,10 @@ import org.springframework.stereotype.Component;
 public class SimpleTimer extends RouteBuilder {
     @Override
     public void configure() throws Exception {
-            from("timer://simpleTimer?period=2000")
-                    .log(LoggingLevel.INFO,"HelloWorld")
-                    .to("log:first-timer");
+            from("timer:simpleTimer?period=1000")
+                    .routeId("simpleTimerId")
+                    .log(LoggingLevel.INFO,"HelloWorld");
+                    //.to("log:first-timer");
 
     }
 }
